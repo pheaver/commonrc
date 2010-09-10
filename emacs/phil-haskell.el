f459a05e8148e59c712e0570ef91009a6b97d400
Philip Weaver <philip.weaver@gmail.com>
2010-09-10 04:43:27 -0700

    phil-haskell.el: cleanup and fix

--- emacs/phil-haskell.el
+++ emacs/phil-haskell.el
@@ -1,43 +1,45 @@
 ;; ----------------------------------------
 
-(when (load "haskell-site-file" 'noerror)
+(load "haskell-site-file" 'noerror)
 
-  (add-hook 'haskell-mode-hook 'turn-on-font-lock)
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-  ;(add-hook 'haskell-mode-hook 'turn-on-haskell-ghci)
-
-  ;; choose one of these indentation modes:
-  ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-  ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-  (setq haskell-indent-offset 2)
-
-  (global-set-key (kbd "C-c h") 'haskell-hoogle)
-
-  ;(setq haskell-literate-default 'latex)
-)
+; hpaste
+(autoload 'hpaste-paste-buffer "hpaste" "hpaste" t)
+(autoload 'hpaste-paste-region "hpaste" "hpaste" t)
 
 (autoload 'haskell-sort-imports "haskell-sort-imports" "haskell-sort-imports" t)
 (autoload 'haskell-align-imports "haskell-align-imports" "haskell-align-imports" t)
 
-(define-key haskell-mode-map (kbd "C-c .")
-  (lambda ()
-    (interactive)
-    (let ((col (current-column))) ;; Save the column.
-      (haskell-align-imports)
-      (haskell-sort-imports)
-      (goto-char (+ (point) col))))) ;; Restore the column.
+(defun my-haskell-cleanup-imports ()
+  (interactive)
+  (let ((col (current-column))) ;; Save the column.
+    (haskell-align-imports)
+    (haskell-sort-imports)
+    (goto-char (+ (point) col)))) ;; Restore the column.
+
+(eval-after-load "haskell-mode"
+  '(progn
+     (define-key haskell-mode-map (kbd "C-c h") 'haskell-hoogle)
+     (define-key haskell-mode-map (kbd "C-c .") 'my-haskell-cleanup-imports)))
+
+(add-hook 'haskell-mode-hook 'turn-on-font-lock)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;(add-hook 'haskell-mode-hook 'turn-on-haskell-ghci)
+
+;; choose one of these indentation modes:
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+
+(setq haskell-indent-offset 2)
+
+;(setq haskell-literate-default 'latex)
 
 (push '("\\.[hg]s$" . haskell-mode) auto-mode-alist)
 (push '("\\.hi$" . haskell-mode) auto-mode-alist)
 (push '("\\.l[hg]s$" . literate-haskell-mode) auto-mode-alist)
 (push '("\\.lang" . haskell-mode) auto-mode-alist)
 (push '("\\.monad" . haskell-mode) auto-mode-alist)
-
-; hpaste
-(autoload 'hpaste-paste-buffer "hpaste" "hpaste" t)
-(autoload 'hpaste-paste-region "hpaste" "hpaste" t)
 
 ;; ----------------------------------------
 
