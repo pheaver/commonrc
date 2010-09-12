7ff3c54262cbcb1bd431999375e1025331678182
Philip Weaver <philip.weaver@gmail.com>
2010-09-11 20:31:15 -0700

    put the initial path stuff in phil-paths.el

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -5,30 +5,7 @@
 (setq inhibit-startup-screen 1)
 (setq initial-scratch-message nil)
 
-;;;; initial stuff
-(when (not (boundp 'user-emacs-directory))
-  (setq user-emacs-directory "~/.emacs.d/"))
-
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(load custom-file 'noerror)
-
-(setq commonrc-dir (file-name-directory load-file-name))
-
-(defun commonrc (&optional path)
-  (if (null path)
-      commonrc-dir
-      (concat commonrc-dir path)))
-
-(defun add-load-path (path)
-  (let ((default-directory path))
-    (add-to-list 'load-path (abbreviate-file-name path))
-    (normal-top-level-add-subdirs-to-load-path)))
-
-(add-load-path "~/local/share/emacs/site-lisp")
-(add-load-path commonrc-dir)
-(add-load-path user-emacs-directory)
-
-(provide 'phil-paths)
+(load-file (concat (file-name-directory load-file-name) "/" "phil-paths.el"))
 
 ;;;; load my other files
 (require 'phil-auto-complete)
