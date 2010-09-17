d76cee60410d2f38eb7df4fb1d1048edb299d22c
Philip Weaver <philip.weaver@gmail.com>
2010-09-17 12:09:41 -0700

    rewrite a lot of stuff in phil-tags.el

--- emacs/phil-tags.el
+++ emacs/phil-tags.el
@@ -9,25 +9,41 @@
   (make-local-variable 'tags-table-list)
   (let ((filename1 (or filename (buffer-file-name)))
         (mode-name (substring (symbol-name major-mode) 0 -5)))
-    (make-tags-table-list (abbreviate-file-name filename1) mode-name)))
+    (make-tags-table-list commonrc-dir t mode-name)
+    (make-tags-table-list (abbreviate-file-name filename1) t mode-name)))
 
 ;; search for TAGS file in all parent directories of path,
 ;; and return a list that can be added to tags-table-list.
-(defun make-tags-table-list (path &optional ext)
+(defun make-tags-table-list (path &optional parents ext)
+  "Search PATH for TAGS files and add to `tags-table-list'.
+
+If PARENTS is an integer, then search that number of parent directories of PATH.
+If PARENTS is t, then search all parent directories of PATH.
+
+If EXT is non-nil, then also search for TAGS.EXT in each directory, in addition
+to TAGS."
+
   (interactive)
-  (setq result-dirs nil)
-  (let ((dirs (reverse
-               (append (parent-dirs (abbreviate-file-name path))
-                       (parent-dirs (abbreviate-file-name (commonrc "."))))))
-        (suffixes (if (null ext) '("") `("" ,(concat "." ext)))))
+  (let ((all-dirs (parent-dirs path))
+        (suffixes (if (null ext) '("") `("" ,(concat "." ext))))
+        (dirs nil))
+    (if parents
+        (if (equal parents t)
+            (setq dirs (reverse all-dirs))
+          (dotimes (n (min parents (length all-dirs)))
+            (push (pop all-dirs dirs))))
+      nil)
+
+    (when (file-directory-p path)
+      (push path dirs))
+
     (dolist (dir dirs)
       (let ((file1 (concat dir "TAGS"))
             (file2 (and ext (concat dir "TAGS" "." ext))))
         (when (file-exists-p file1)
           (add-to-list 'tags-table-list file1))
         (when (and file2 (file-exists-p file2))
-          (add-to-list 'tags-table-list file2)))))
-  result-dirs)
+          (add-to-list 'tags-table-list file2))))))
 
 (defun my-find-tag ()
   (interactive)
@@ -37,7 +53,7 @@
                  'find-tag-default))))
 
 (eval-after-load "etags"
-  '(make-tags-table-list (commonrc ".")))
+  '(make-tags-table-list commonrc-dir t))
 
 (global-set-key "\M-]" 'my-find-tag)
 (global-set-key "\M-[" 'pop-tag-mark)
