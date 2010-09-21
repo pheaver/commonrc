98bbafd15ccd8a191ca32f325835a699a8f3d531
Philip Weaver <philip.weaver@gmail.com>
2010-09-21 14:39:12 -0700

    lots of work on phil-auto-complete.el
    - autoload 'auto-complete and bind C-c TAB to 'auto-complete.
    
    - add support for when auto-complete is started after emacs is initialized, by
      checking the current mode and calling any ac-setup-* functions in
      phil-auto-complete-setup.
    
    - in general, support loading etags/auto-complete/verilog/haskell modes in any
      order and still have it all work.

--- emacs/phil-auto-complete.el
+++ emacs/phil-auto-complete.el
@@ -11,33 +11,58 @@
     ac-source-files-in-current-dir
     ))
 
+(autoload 'auto-complete "auto-complete" nil t)
+(global-set-key (kbd "C-c TAB") 'auto-complete)
+
+(defun ac-etags-setup ()
+  (add-to-list 'ac-sources 'ac-source-etags 'append))
+
+(defun ac-verilog-setup ()
+  (add-to-list 'ac-sources 'ac-source-verilog))
+
+(defun ac-haskell-setup ()
+  (when (featurep 'ghc)
+    (add-to-list 'ac-sources 'ac-source-ghc-mod)))
+
 (defun phil-auto-complete-setup ()
   (interactive)
   (require 'auto-complete)
   (require 'auto-complete-config)
 
   (add-to-list 'ac-dictionary-directories (commonrc "auto-complete.git/dict"))
-
-  (when (and (require 'etags nil t) (require 'auto-complete-etags nil t))
-    (add-to-list 'my-default-ac-sources 'ac-source-etags 'append)
-    (set-face-foreground 'ac-etags-candidate-face "orange")
-    (setq ac-etags-ignore-empty-table-list t)
-    )
-
-  (ac-config-default)
-  (setq-default ac-sources my-default-ac-sources)
-
   (add-to-list 'ac-modes 'makefile-gmake-mode)
 
-  (add-hook 'haskell-mode-hook
-     (lambda ()
-       (when (featurep 'ghc) ;; (require 'ghc-mod nil t)
-         (add-to-list 'ac-sources 'ac-source-ghc-mod))))
+  (setq-default ac-sources my-default-ac-sources)
 
-  (eval-after-load "verilog"
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  (when (memq major-mode '(emacs-lisp-mode lisp-interaction-mode inferior-emacs-lisp-mode))
+    (ac-emacs-lisp-mode-setup))
+
+  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (when (equal major-mode 'c-mode) (ac-cc-mode-setup))
+
+  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+  (when (equal major-mode 'ruby-mode) (ac-ruby-mode-setup))
+
+  (add-hook 'css-mode-hook 'ac-css-mode-setup)
+  (when (equal major-mode 'css-mode) (ac-css-mode-setup))
+
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (ac-common-setup)
+
+  (when (and (require 'etags nil t) (require 'auto-complete-etags nil t))
+    (set-face-foreground 'ac-etags-candidate-face "orange")
+    (setq ac-etags-ignore-empty-table-list t)
+    (add-hook 'auto-complete-mode-hook 'ac-etags-setup)
+    (ac-etags-setup))
+
+  (add-hook 'haskell-mode-hook 'ac-haskell-setup)
+  (when (equal major-mode 'haskell-mode) (ac-haskell-setup))
+
+  (eval-after-load 'verilog-mode
     (when (require 'auto-complete-verilog nil t)
-      (add-hook 'verilog-mode-hook
-                '(lambda () (add-to-list 'ac-sources 'ac-source-verilog)))))
+      (add-hook 'verilog-mode-hook 'ac-verilog-setup)
+      (when (equal major-mode 'verilog-mode) (ac-verilog-setup))))
 
   ;; this is default.  it makes TAB cycle through menu elements
   ;; (define-key ac-completing-map (kbd "TAB") 'ac-expand)
@@ -62,10 +87,15 @@
 
   ;; enable fuzzy matching
   (setq ac-fuzzy-enable t)
+
+  (global-auto-complete-mode t)
   )
 
+(eval-after-load 'auto-complete
+  '(phil-auto-complete-setup))
+
 (when (and (fboundp 'daemonp) (daemonp))
-  (phil-auto-complete-setup))
+  (require 'auto-complete nil t))
 
 ;; ----------------------------------------
 
