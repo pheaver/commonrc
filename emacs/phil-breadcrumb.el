487648275163c49ca910e39c1375a6f5e1d431d6
Philip Weaver <philip.weaver@gmail.com>
2010-09-21 17:55:01 -0700

    update breadcrumb.git

--- emacs/phil-breadcrumb.el
+++ emacs/phil-breadcrumb.el
@@ -1,14 +1,7 @@
 ;; ----------------------------------------
 ;; my breadcrumb settings
 
-(autoload 'bc-set            "breadcrumb" nil t)
-(autoload 'bc-previous       "breadcrumb" nil t)
-(autoload 'bc-next           "breadcrumb" nil t)
-(autoload 'bc-local-previous "breadcrumb" nil t)
-(autoload 'bc-local-next     "breadcrumb" nil t)
-(autoload 'bc-goto-current   "breadcrumb" nil t)
-(autoload 'bc-list           "breadcrumb" nil t)
-(autoload 'bc-clear          "breadcrumb" nil t)
+(load "breadcrumb-site-file")
 
 (global-set-key (kbd "C-c SPC") 'bc-set)
 (global-set-key (kbd "C-c p"  ) 'bc-previous)
