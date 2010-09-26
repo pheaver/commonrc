d2e86a916df201fb8b497e752396cbdafff5523d
Philip Weaver <philip.weaver@gmail.com>
2010-09-26 02:47:45 -0700

    update phil-frames.el

--- emacs/phil-frames.el
+++ emacs/phil-frames.el
@@ -16,27 +16,25 @@
 (defun resize-current-frame () (interactive)
   (resize-frame (selected-frame)))
 
-(add-hook 'after-make-frame-functions 'phil-new-frame-hook)
-;;(add-hook 'window-setup-hook 'maximize-frame t)
-
 (when (fboundp 'ns-toggle-fullscreen)
   (global-set-key (kbd "<s-return>") 'ns-toggle-fullscreen))
 
-(when (fboundp 'ns-do-applescript)
-  (defun ns-raise-emacs ()
-    (ns-do-applescript "tell application \"Emacs\" to activate")))
+(defun phil/ns-raise-emacs ()
+  (ns-do-applescript "tell application \"Emacs\" to activate"))
 
-(defun phil-new-frame-hook (frame)
+(defun phil/new-frame-hook (frame)
   (interactive)
   (select-frame frame)
   (raise-frame frame)
   (let ((x (framep frame)))
     (when (equal x 'ns)
-      (require 'phil-darwin)
-      (ns-raise-emacs)
+      (phil/ns-raise-emacs)
       (when (require 'maxframe "maxframe" 'noerror)
         (sleep-for 0 10)
         (maximize-frame)))))
+
+(add-hook 'after-make-frame-functions 'phil/new-frame-hook)
+;;(add-hook 'window-setup-hook 'maximize-frame t)
 
 ;; ----------------------------------------
 
