a241721daa42ec7527f88778f3aae394d280cd32
Philip Weaver <philip.weaver@gmail.com>
2012-01-19 20:51:12 -0800

    update phil-frames.el with solarized-dark and some custom colors.

--- emacs/phil-frames.el
+++ emacs/phil-frames.el
@@ -6,6 +6,40 @@
 ;;                            nil
 ;;                          'fullboth)))
 
+(setq color-theme-is-global nil)
+
+(add-load-path "~/local/src/solarized/emacs-colors-solarized")
+;; (autoload 'color-theme-solarized-light "color-theme-solarized")
+;; (autoload 'color-theme-solarized-dark "color-theme-solarized")
+
+(defun phil/set-face-attribute ( face frame &rest args)
+  (if (facep face) (apply 'set-face-attribute face frame args)))
+
+(defun phil/set-frame-theme (&optional frame)
+  (interactive)
+
+  ;; these break for any face attributes that aren't yet defined
+  (phil/set-face-attribute 'jabber-activity-face frame
+                      :foreground "orangered3" :weight 'bold)
+  (phil/set-face-attribute 'jabber-activity-personal-face frame
+                      :foreground nil :inherit 'outline-1 :weight 'bold)
+  (phil/set-face-attribute 'jabber-chat-prompt-foreign frame
+                      :foreground nil :inherit 'outline-2 :weight 'bold)
+  (phil/set-face-attribute 'jabber-chat-prompt-local frame
+                      :foreground nil :inherit 'outline-1 :weight 'bold)
+  (phil/set-face-attribute 'jabber-chat-prompt-system frame
+                      :foreground nil :inherit 'outline-3 :weight 'bold)
+  (phil/set-face-attribute 'magit-item-highlight frame
+                      :foreground nil :inherit 'highlight)
+  (phil/set-face-attribute 'outline-2 frame
+                      :inherit 'font-lock-variable-name-face :foreground "brown")
+  (phil/set-face-attribute 'diff-changed frame :inverse-video nil)
+  (phil/set-face-attribute 'diff-added frame :inverse-video nil)
+  (phil/set-face-attribute 'diff-removed frame :inverse-video nil)
+  (phil/set-face-attribute 'diff-header frame :background nil)
+  (phil/set-face-attribute 'diff-file-header frame :background nil)
+  )
+
 (defun resize-frame (frame) (interactive)
   (progn
     (set-frame-position frame 0 0)
@@ -26,6 +60,13 @@
   (interactive)
   (select-frame frame)
   (raise-frame frame)
+  (set-variable 'color-theme-is-global nil)
+
+  (when (and window-system
+             (require 'color-theme-solarized nil 'noerror))
+    (color-theme-solarized-dark)
+    (phil/set-frame-theme frame))
+
   (let ((x (framep frame)))
     (when (equal x 'ns)
       (phil/ns-raise-emacs)
