7521473af589b1159ecdc661280e29fddd15938c
Philip Weaver <philip.weaver@gmail.com>
2016-03-12 11:54:42 -0800

    remove magit-quit-buffers, because magit-for-all-buffers no longer exists.

--- emacs/phil-vc.el
+++ emacs/phil-vc.el
@@ -51,13 +51,6 @@
 ;; for older versions of magit
 (setq magit-repo-dirs magit-repository-directories)
 
-(defun magit-quit-buffers ()
-  (interactive)
-  (magit-for-all-buffers 'bury-buffer))
-
-(eval-after-load 'magit
-  '(define-key magit-mode-map (kbd "Q") 'magit-quit-buffers))
-
 ;; over ssh on CentOS, C-/ behaves as C-_
 (global-set-key (kbd "C-x C-/") 'magit-status)
 (global-set-key (kbd "C-x C-_") 'magit-status)
