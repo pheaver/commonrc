0cc7a1d6b9de03e42c8c3d5e64643333545fd026
Philip Weaver <philip.weaver@gmail.com>
2017-01-14 12:23:06 -0800

    eclim settings.

--- emacs/phil-java.el
+++ emacs/phil-java.el
@@ -4,14 +4,19 @@
 (add-hook 'java-mode-hook 'phil/java-mode-hook)
 
 (with-eval-after-load 'eclim
-  (global-eclim-mode 1)
-  (setq eclim-auto-save nil)
-  (setq eclim-executable "/home/pweaver/eclipse/eclim")
+  (setq eclim-auto-save t)
+  (setq eclimd-autostart-with-default-workspace t)
+  (setq eclimd-autostart t)
+
+  (global-eclim-mode)
+
   (define-key eclim-mode-map (kbd "M-n") 'eclim-problems-next-same-window)
   (define-key eclim-mode-map (kbd "M-p") 'eclim-problems-previous-same-window)
   (define-key eclim-mode-map (kbd "M-c") 'eclim-problems-correct)
   (define-key eclim-mode-map (kbd "M-]") 'eclim-java-find-declaration)
-  (require 'company-emacs-eclim)
+  (require 'company-emacs-eclim nil t))
+
+(with-eval-after-load 'company-emacs-eclim
   (company-emacs-eclim-setup))
 
 (defun load-eclim ()
