ef75473c5ca7ac212f1b4f1caa08bbf71a71ed91
Philip Weaver <philip@tripshot.com>
2017-11-22 11:16:33 -0800

    get i3 volume control working.

---
+++ bin/set_mute.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+arg="$1"
+
+n=0
+
+# just select the last card in the list (which I assume is the highest number, and probably the headset/headphones if plugged in).
+for x in $(pactl list sinks short | cut -f 1); do
+  n=$x
+done
+
+pactl set-sink-mute $n $arg
